10cfdc3f87c35c99fa10f8cee9e361400d55138b
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-03-11 23:38:06 -0500

    up

--- Control/colors.py
+++ Control/colors.py
@@ -3,7 +3,9 @@ import cv2
 cap=cv2.VideoCapture(0)
 while True:
     ret,frame=cap.read()
+    hvs_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     cv2.imshow("frame",frame)
     key=cv2.waitKey(1)
     if key==27:
-        
+        break
+
