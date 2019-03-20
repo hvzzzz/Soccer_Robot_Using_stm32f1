ba8098524d2c6b371889bf894fa2166861c8f057
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-03-19 19:20:22 -0500

    created trackbars!!!!

---
+++ Control/track.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+cap=cv2.VideoCapture(0)
+#trackbars
+def nothing(x):
+    pass
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H","Trackbars",0,179,nothing)
+cv2.createTrackbar("L-S","Trackbars",0,255,nothing)
+cv2.createTrackbar("L-V","Trackbars",0,255,nothing)
+cv2.createTrackbar("U-H","Trackbars",0,179,nothing)
+cv2.createTrackbar("U-S","Trackbars",0,255,nothing)
+cv2.createTrackbar("U-V","Trackbars",0,255,nothing)
+while True:
+    ret,frame=cap.read()
+    hsv_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    l_h=cv2.getTrackbarPos("L-H","Trackbars")
+    l_s=cv2.getTrackbarPos("L-S","Trackbars")
+    l_v=cv2.getTrackbarPos("L-V","Trackbars")
+    u_h=cv2.getTrackbarPos("U-H","Trackbars")
+    u_s=cv2.getTrackbarPos("U-S","Trackbars")
+    u_v=cv2.getTrackbarPos("U-V","Trackbars")
+    lower_blue=np.array([l_h,l_s,l_v])
+    upper_blue=np.array([u_h,u_s,u_v])
+    mask=cv2.inRange(hsv_frame,lower_blue,upper_blue)
+    cv2.imshow("frame",frame)
+    cv2.imshow("mask",mask)
+    key=cv2.waitKey(1)
+    if key==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
