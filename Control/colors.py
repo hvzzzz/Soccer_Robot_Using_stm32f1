e01211f724256c06a23d24dbba5c150153b96bf5
hvzzzz <42120805+hvzzzz@users.noreply.github.com>
2019-03-24 19:58:32 -0500

    now can detect more than one color

--- Control/colors.py
+++ Control/colors.py
@@ -9,17 +9,21 @@ while True:
     #green_hvs = cv2.cvtColor( np.uint8([[bgr]] ), cv2.COLOR_BGR2HSV)[0][0]
     hvs_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     #red ones
-    low_red =np.array([0,19 ,0])
-    high_red =np.array([113,221,156])
+    low_red =np.array([0,110 ,156])
+    high_red =np.array([14,231,255])
     #green ones
     #green_hvs=np.array([108,56,56])
-    #minHSV = np.array([green_hvs[0] - thresh, green_hvs[1] - thresh, green_hvs[2] - thresh])
-    #maxHSV = np.array([green_hvs[0] + thresh, green_hvs[1] + thresh, green_hvs[2] + thresh])
-    mask=cv2.inRange(hvs_frame,low_red,high_red)
+    minHSV = np.array([73,49,70])
+    maxHSV = np.array([90,171,202])
+    maskr=cv2.inRange(hvs_frame,low_red,high_red)
+    #red = cv2.bitwise_and(frame,frame,mask=maskr)
+    maskgreen=cv2.inRange(hvs_frame,minHSV,maxHSV)
+    mask=maskgreen|maskr
     red = cv2.bitwise_and(frame,frame,mask=mask)
-    #maskgreen=cv2.inRange(brightHSV,minHSV,maxHSV)
     cv2.imshow("frame",frame)
     cv2.imshow("Detected",red)
+    #print(maskr.shape)
+    #print(frame.shape)
     key=cv2.waitKey(1)
     if key==27:
         break
